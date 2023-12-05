149f376bffad3840d6fede1098aad5814da786fc
ChittreshM <chittresh15512@gmail.com>
2023-12-05 14:01:06 -0800

    Aarav stfu

---
+++ main.py
@@ -0,0 +1,6 @@
+import turtle 
+import random
+
+turtle.bgpic("ChristmasBG.gif")
+secret_words = ["christmas", "candy cane", "snowman", "santa", "reindeer", "naughty list", "elf", "snowflake", "snowman", ]
+hangman = turtle.Turtle()
